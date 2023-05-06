916ff03ce6a58609a6a46613f23dc958542dc318
NestorCava <nestorcavallini@gmail.com>
2023-05-06 19:20:14 -0300

    Función edit en los datos personales

--- src/app/components/portfolio-header/portfolio-header.component.ts
+++ src/app/components/portfolio-header/portfolio-header.component.ts
@@ -27,4 +27,17 @@ export class PortfolioHeaderComponent implements OnInit{
     this.persona=persona;
     this.updatePersona.emit(this.persona);
   }
+
+  onEditDatos(persona: Persona){
+    persona.nombre=(document.getElementById("nombre-portfolio-header"+persona.id))
+                                            ?.textContent as string;
+    persona.apellido=(document.getElementById("apellido-portfolio-header"+persona.id))
+                                            ?.textContent as string;
+    persona.posicion=(document.getElementById("posicion-portfolio-header"+persona.id))
+                                            ?.textContent as string;
+    persona.localidad=(document.getElementById("localidad-portfolio-header"+persona.id))
+                                            ?.textContent as string;
+                    
+    this.editPersona(persona);
+  }
 }
